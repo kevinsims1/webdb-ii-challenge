f76d167cfd493c5b2346cf7826782c6ef44a7ff5
Kevin Sims <kevinsims1@outlook.com>
2019-07-16 16:26:18 -0700

    stretch and mvp

--- knexfile.js
+++ knexfile.js
@@ -5,17 +5,17 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: '/data/car-dealer.db3'
+      filename: './data/car-dealer.db3'
     },
 
-    useNullAsDefault: true
-  },
+    useNullAsDefault: true,
 
-  migrations: {
-    directory: './data/migrations'
-  },
-
-  seeds: {
-    directory: './data/seeds'
+    migrations: {
+      directory: './data/migrations'
+    },
+  
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
